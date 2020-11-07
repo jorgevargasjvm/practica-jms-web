9299ac1540312383bc6207b18260278209fa29a6
Jorge Vargas <jorgevargasmateo@hotmail.com>
2020-11-07 16:21:06 -0400

    initial commit

---
+++ src/main/java/com/vargas/PracticaJMSWeb/PracticaJmsWebApplication.java
@@ -0,0 +1,13 @@
+package com.vargas.PracticaJMSWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaJmsWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaJmsWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vargas/PracticaJMSWeb/PracticaJmsWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vargas.PracticaJMSWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaJmsWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
